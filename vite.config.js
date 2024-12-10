038291385cb4951734ca59bd0a6bb34351d60ae1
mohd-shan <mohdshan.me@gmail.com>
2024-12-10 17:02:30 +0530

    fix base path mismatch

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', 'VITE_');
   return {
-    base: env.VITE_BASE_PATH || "/resume",
+    base: env.VITE_BASE_PATH || "/resume/",
     plugins: [react()],
   };
 });
