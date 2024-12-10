213519e87788307259a1789a2f29bcf5f5ff69eb
mohd-shan <mohdshan.me@gmail.com>
2024-12-10 14:00:51 +0530

    add base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/resume',
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', 'VITE_');
+  return {
+    base: env.VITE_BASE_PATH || "/resume",
+    plugins: [react()],
+  };
+});
